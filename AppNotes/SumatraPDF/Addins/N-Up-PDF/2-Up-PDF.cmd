d446fa003a4ba1cd5ebbfba0a9a208f8e03e64b6
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2021-03-13 22:01:47 +0000

    include -view facing

--- AppNotes/SumatraPDF/Addins/N-Up-PDF/2-Up-PDF.cmd
+++ AppNotes/SumatraPDF/Addins/N-Up-PDF/2-Up-PDF.cmd
@@ -1,4 +1,4 @@
-@ echo off & echo  & Mode 82,18 & color E0 & Title SumatraPDF-Addin 2-Up-PDF v'21-03-13--01
+@ echo off & echo  & Mode 82,18 & color E0 & Title SumatraPDF-Addin 2-Up-PDF v'21-03-13--02
 SetLocal EnableDelayedExpansion &: Cpdf Alternative 2-UP version using cpdf -twoup in.pdf -o out.pdf 
 : 
 : NOTE this file is not as well documented as to how "addins" work so for now read one of the others 
@@ -22,7 +22,7 @@ goto 2-Up-PDF
 echo: & echo  Usage:  %~n0 "path\filename", offers 2-Up a PDF as  Filename-2-Up.pdf
 echo:
 echo  Special no prompt version ONLY 2-Up but attempts to retain Bookmarks and
-echo  internal links. Not for comercial use unless you license Cpdf
+echo internal links. Not for comercial use unless you license Cpdf
 echo:
 echo  Note Landscape and Portrait pages will differ, do not use mixed.
 echo  Intended for printing. HYPERLINKS may break, SumatraPDF may still use some.
@@ -39,7 +39,7 @@ echo: & echo   Processing %1 as 2-Up
 echo: & "%~dp0..\..\Utils\cpdf\cpdf" -twoup "%~dpn1.pdf" -o "%~dpn1-2-Up-tmp.pdf" >nul
 echo: & "%~dp0..\..\Utils\cpdf\cpdf" "%~dpn1-2-Up-tmp.pdf" -rotate 90 -o "%~dpn1-2-Up.pdf" >nul
 if exist "%~dpn1-2-Up.pdf" del "%~dpn1-2-Up-tmp.pdf"
-echo: & start "" "%addins%\..\SumatraPDF.exe" -reuse-instance "%~dpn1-2-Up.pdf"
+echo: & start "" "%addins%\..\SumatraPDF.exe" -reuse-instance -view facing "%~dpn1-2-Up.pdf"
 
 : remove these two lines if you dont want to check progress
 echo: & timeout /t 8
