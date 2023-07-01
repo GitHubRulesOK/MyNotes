b0b6ee12b1d77c23a560adca8a024ba10cca24a9
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2023-07-01 04:15:28 +0100

    Add files via upload

--- JScriptSamples/HWB&Wstream.js
+++ JScriptSamples/HWB&Wstream.js
@@ -6,7 +6,7 @@ BS.Open();
 BS.Position = 0;
 
 BS.WriteText("%PDF-1.0\n");
-BS.WriteText("%þÞÚÛ\n");
+BS.WriteText("%ÅÑ§¡\n");
 
 BS.WriteText("1 0 obj <</Type/Catalog/Pages 2 0 R>> endobj\n");
 BS.WriteText("2 0 obj <</Type/Pages/Count 1/Kids[3 0 R]>> endobj\n");
@@ -14,12 +14,14 @@ BS.WriteText("3 0 obj <</Type/Page/MediaBox[0 0 144 144]/Rotate 0/Resources<</XO
 BS.WriteText("4 0 obj <</Type/XObject/Subtype/Image/Height 25/Width 24/BitsPerComponent 1/Length 75/ColorSpace/DeviceGray>> stream\n");
 BS.WriteText('ÿÿÿÿÿÿÀmß[}ÑoEÑ[EÑqEßE}ÀUÿñÿÁ«Á¬ÛZcýÖÇÈ"}ÿÕïÀMsß`§Ñ]9ÑNÑE·ßLÇÀA[ÿÿÿÿÿÿ');
 BS.WriteText("\nendstream\nendobj\n");
+var Pos1 = "000000000"+BS.Position
 BS.WriteText("5 0 obj <</Length 101>> stream\n");
 BS.WriteText("q\n1 0 0 -1 18 54 cm\n35 0 0 -36 0 36 cm\n/Img0 Do\nQ\nq\n1 0 0 -1 71 144 cm\n70 0 0 -72 0 72 cm\n/Img0 Do\nQ\n");
 BS.WriteText("\nendstream\nendobj\n");
+var Pos2 = BS.Position
 BS.WriteText("\nxref\n0 6\n");
-BS.WriteText("0000000000 00001 f \n0000000015 00000 n \n0000000060 00000 n \n0000000111 00000 n \n0000000237 00000 n \n0000000447 00000 n \n");
-BS.WriteText("\ntrailer\n<</Size 6/Info<</Producer(JScrip2pdf)>>/Root 1 0 R>>\nstartxref\n598\n%%EOF\n");
+BS.WriteText("0000000000 00001 f \n0000000015 00000 n \n0000000060 00000 n \n0000000111 00000 n \n0000000237 00000 n \n"+Pos1.slice(-10)+" 00000 n \n");
+BS.WriteText("\ntrailer\n<</Size 6/Info<</Producer(JScrip2pdf)>>/Root 1 0 R>>\nstartxref\n"+Pos2+"\n%%EOF\n");
 
 BS.SaveToFile("HelloWorldB&W.pdf", 2);
 BS.Close();
